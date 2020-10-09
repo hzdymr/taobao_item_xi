1b49993e9743f512ed9388c01c29a9558017200b
hzdymr <215672097@qq.com>
2020-10-09 22:02:50 +0800

    首页结构已经完成，js还差懒加载、楼梯和渲染

--- src/script/index1xuanran.js
+++ src/script/index1xuanran.js
@@ -519,7 +519,10 @@ xuanran(1, `<span>
 </span>`, '.app section')
     // 有好货渲染
 xuanran(5, `<div>
-<img src="https://img.alicdn.com/imgextra/i1/2959008325/TB2zh2RoRjTBKNjSZFNXXasFXXa_!!2959008325-0-beehive-scenes.jpg_360x360xzq90.jpg_.webp" alt="">
+<section class="img-shadow">
+    <img src="https://img.alicdn.com/imgextra/i1/2959008325/TB2zh2RoRjTBKNjSZFNXXasFXXa_!!2959008325-0-beehive-scenes.jpg_360x360xzq90.jpg_.webp" alt="">
+    <section></section>
+</section>
 <h4>Bite 唇釉</h4>
 <p>简洁的深灰色包装，低调但不凡。</p>
 <p class="extra">
@@ -529,16 +532,34 @@ xuanran(5, `<div>
     //每日好店渲染
 xuanran(3, `<div>
 <h5>万能市集</h5>
-<img src="https://img.alicdn.com/bao/uploaded/i4/50079713/O1CN011QQXyt2LcbY1Tz3RW_!!50079713.jpg_350x350q90.jpg_.webp" alt="">
-<img src="https://img.alicdn.com/bao/uploaded/i4/50079713/O1CN01989rpQ2LcbXx2jAzZ_!!50079713.jpg_170x170q90.jpg_.webp" alt="">
-<img src="https://img.alicdn.com/bao/uploaded/i4/50079713/O1CN01W9y4ZV2LcbYApX83x_!!50079713.jpg_170x170q90.jpg_.webp" alt="">
+<section class="img-shadow">
+    <img src="https://img.alicdn.com/bao/uploaded/i4/50079713/O1CN011QQXyt2LcbY1Tz3RW_!!50079713.jpg_350x350q90.jpg_.webp" alt="">
+    <section></section>
+</section>
+<section class="img-shadow">
+    <img src="https://img.alicdn.com/bao/uploaded/i4/50079713/O1CN01989rpQ2LcbXx2jAzZ_!!50079713.jpg_170x170q90.jpg_.webp" alt="">
+    <section></section>
+</section>
+<section class="img-shadow">
+    <img src="https://img.alicdn.com/bao/uploaded/i4/50079713/O1CN01W9y4ZV2LcbYApX83x_!!50079713.jpg_170x170q90.jpg_.webp" alt="">
+    <section></section>
+</section>
 </div>`, '.meiri-left .meiri-bottom')
     //淘宝直播渲染
 xuanran(3, `<div>
 <h5>青青河边草真皮<span>25788观看</span></h5>
-<img src="https://img.alicdn.com/imgextra/i2/6000000005008/O1CN01pCSLGm1mri7m8ta1Y_!!6000000005008-0-tblive.jpg_320x320q90.jpg_.webp" alt="">
-<img src="https://img.alicdn.com/bao/uploaded/i4/10840352/O1CN01cS0YA31ETG2N03t3a_!!10840352.jpg_170x170q90.jpg_.webp" alt="">
-<img src="https://img.alicdn.com/bao/uploaded/i4/10840352/O1CN01MSX4Tm1ETG2bXRLVR_!!10840352.jpg_170x170q90.jpg_.webp" alt="">
+<section class="img-shadow">
+    <img src="https://img.alicdn.com/imgextra/i2/6000000005008/O1CN01pCSLGm1mri7m8ta1Y_!!6000000005008-0-tblive.jpg_320x320q90.jpg_.webp" alt="">
+    <section></section>
+</section>
+<section class="img-shadow">
+    <img src="https://img.alicdn.com/bao/uploaded/i4/10840352/O1CN01cS0YA31ETG2N03t3a_!!10840352.jpg_170x170q90.jpg_.webp" alt="">
+    <section></section>
+</section>
+<section class="img-shadow">
+    <img src="https://img.alicdn.com/bao/uploaded/i4/10840352/O1CN01MSX4Tm1ETG2bXRLVR_!!10840352.jpg_170x170q90.jpg_.webp" alt="">
+    <section></section>
+</section>
 <h6>我发誓:专柜大牌真皮女鞋29元起</h6>
 </div>`, '.zhibo .meiri-bottom')
     //特色好物渲染
@@ -597,4 +618,268 @@ xuanran(3, `<li><a href="javascript:;">关于淘宝</a></li>
 <li><a href="javascript:;">营销中心</a></li>`, '.footer-middle>ul')
 xuanran(4, `<a href="javascript:;">增值电信业务经营许可证：浙B2-20080224</a><span>|</span>
 <a href="javascript:;">增值电信业务经营许可证（跨地区）： B2-20150210</a><span>|</span>
-<a href="javascript:;">浙网文（2019）1033-086号</a><span>|</span>`, '.footer-middle>div')
+<a href="javascript:;">浙网文（2019）1033-086号</a><span>|</span>`, '.footer-middle>div');
+
+
+
+// 其他效果
+// 轮播图1
+function getStyle(obj, attr) { //obj:运动的物体   attr:运动物体的属性
+    if (window.getComputedStyle) { //标准
+        return window.getComputedStyle(obj)[attr];
+    } else {
+        return obj.currentStyle[attr];
+    }
+}
+
+function bufferMove(obj, json, fn) { //obj:运动的物体   json：属性对象   fn:函数(函数做参数，回调函数)
+    let speed = 0;
+    clearInterval(obj.timer);
+    obj.timer = setInterval(() => {
+        let flag = true;
+        //遍历属性attr，属性值
+        for (let attr in json) { //attr:属性名   json[attr]:目标值
+            //求当前值(透明度有问题)
+            let currentvalue = null;
+            if (attr === 'opacity') { //透明度
+                currentvalue = getStyle(obj, attr) * 100; //将透明度的值扩大100倍。
+            } else {
+                currentvalue = parseInt(getStyle(obj, attr));
+            }
+            //求缓冲的速度
+            speed = (json[attr] - currentvalue) / 5;
+            speed = speed > 0 ? Math.ceil(speed) : Math.floor(speed);
+
+            //物体的运动和停止
+            if (currentvalue !== json[attr]) { //当前的某个属性没有到目标点，继续运动
+                //透明度有问题,透明度没有单位
+                if (attr === 'opacity') {
+                    obj.style.opacity = (currentvalue + speed) / 100; //缩小100倍，0-1之间的值进行赋值
+                } else {
+                    obj.style[attr] = currentvalue + speed + 'px';
+                }
+                //只有物体还在运动
+                flag = false;
+            }
+        }
+
+        if (flag) { //flag ===true for没有改变flag的值,运动的结束了。
+            clearInterval(obj.timer);
+            fn && typeof fn === 'function' && fn(); //上一次运动停止了 如果fn存在，fn是一个函数，执行fn，否则不执行。
+        }
+
+
+    }, 10)
+}
+
+function carousel() {
+    //获取元素
+    this.carousel = document.querySelector('.carousel');
+    this.list = document.querySelector('.carousel ul');
+    this.picli = document.querySelectorAll('.carousel ul li'); //6张图
+    this.picbtn = document.querySelectorAll('.carousel div i'); //5个圆圈
+    this.leftarrow = document.querySelector('#arrowleft');
+    this.rightarrow = document.querySelector('#arrowright');
+    this.index = 0; //存储索引的变量。
+    this.timer = null;
+
+}
+
+carousel.prototype.init = function() {
+    //1.改变了布局，重新设置ul的width
+    this.list.style.width = this.picli.length * this.picli[0].offsetWidth + 'px';
+    this.liwidth = this.picli[0].offsetWidth; //一个li的宽度。
+    //2.给picbtn添加点击事件。
+    for (let i = 0; i < this.picbtn.length; i++) {
+        this.picbtn[i].onclick = () => {
+            this.index = i - 1;
+            //this.tabswitch有一个this.index++;
+            this.tabswitch()
+        }
+    }
+    //3.左右箭头事件。
+    this.rightarrow.onclick = () => {
+        this.tabswitch();
+    }
+
+    this.leftarrow.onclick = () => {
+            //本身减1  tabswitch有加1
+            this.index -= 2;
+            this.tabswitch();
+        }
+        //4.自动播放
+    this.timer = setInterval(() => {
+        this.rightarrow.onclick();
+    }, 3000);
+
+    this.carousel.onmouseover = () => {
+        clearInterval(this.timer);
+    }
+    this.carousel.onmouseout = () => {
+        this.timer = setInterval(() => {
+            this.rightarrow.onclick();
+        }, 3000);
+    }
+};
+
+//封装一个方法将切换的过程单独封装。
+carousel.prototype.tabswitch = function() {
+    this.index++;
+    if (this.index === this.picbtn.length + 1) { //右箭头
+        this.list.style.left = 0 + 'px'; //从0开始运动完一张。
+        this.index = 1; //第二张图片进来
+    }
+    if (this.index === -1) { //左键头
+        this.list.style.left = -this.liwidth * this.picbtn.length - 1 + 'px';
+        this.index = this.picbtn.length - 1; //索引4
+    }
+    for (let i = 0; i < this.picbtn.length; i++) {
+        this.picbtn[i].className = ''
+    }
+    if (this.index === this.picbtn.length) { //this.picbtn.length:5
+        this.picbtn[0].className = 'active';
+    } else {
+        this.picbtn[this.index].className = 'active';
+    }
+    bufferMove(this.list, {
+        left: -this.index * this.liwidth
+    });
+}
+new carousel().init(); //实例化+调用初始化方法。
+
+// 轮播图2
+function carousel2() {
+    //获取元素
+    this.carousel2 = document.querySelector('.carousel2');
+    this.list = document.querySelector('.carousel2 ul');
+    this.picli = document.querySelectorAll('.carousel2 ul li'); //6张图
+    this.picbtn = document.querySelectorAll('.carousel2 div i'); //5个圆圈
+    this.leftarrow = document.querySelector('#arrowleft1');
+    this.rightarrow = document.querySelector('#arrowright1');
+    this.index = 0; //存储索引的变量。
+    this.timer = null;
+    this.em = document.querySelector('.carousel2 em');
+
+}
+
+carousel2.prototype.init = function() {
+    //1.改变了布局，重新设置ul的width
+    this.list.style.width = this.picli.length * this.picli[0].offsetWidth + 'px';
+    this.liwidth = this.picli[0].offsetWidth; //一个li的宽度。
+    //2.给picbtn添加点击事件。
+    for (let i = 0; i < this.picbtn.length; i++) {
+        this.picbtn[i].onclick = () => {
+            this.index = i - 1;
+            //this.tabswitch有一个this.index++;
+            this.tabswitch()
+        }
+    }
+    //3.左右箭头事件。
+    this.rightarrow.onclick = () => {
+        this.tabswitch();
+    }
+
+    this.leftarrow.onclick = () => {
+            //本身减1  tabswitch有加1
+            this.index -= 2;
+            this.tabswitch();
+        }
+        //4.自动播放
+    this.timer = setInterval(() => {
+        this.rightarrow.onclick();
+    }, 3000);
+
+    this.carousel2.onmouseover = () => {
+        clearInterval(this.timer);
+    }
+    this.carousel2.onmouseout = () => {
+        this.timer = setInterval(() => {
+            this.rightarrow.onclick();
+        }, 3000);
+    }
+};
+
+//封装一个方法将切换的过程单独封装。
+carousel2.prototype.tabswitch = function() {
+    this.index++;
+    if (this.index === this.picbtn.length + 1) { //右箭头
+        this.list.style.left = 0 + 'px'; //从0开始运动完一张。
+        this.index = 1; //第二张图片进来
+    }
+    if (this.index === -1) { //左键头
+        this.list.style.left = -this.liwidth * this.picbtn.length - 1 + 'px';
+        this.index = this.picbtn.length - 1; //索引4
+    }
+    for (let i = 0; i < this.picbtn.length; i++) {
+        this.picbtn[i].className = ''
+    }
+    if (this.index === this.picbtn.length) { //this.picbtn.length:5
+        this.picbtn[0].className = 'active';
+    } else {
+        this.picbtn[this.index].className = 'active';
+    }
+    bufferMove(this.list, {
+        left: -this.index * this.liwidth
+    });
+    if (this.index + 1 > 6) {
+        this.em.innerHTML = 1
+    } else {
+        this.em.innerHTML = this.index + 1;
+    }
+
+}
+new carousel2().init(); //实例化+调用初始化方法。
+
+// tab切换
+! function($) {
+    $('.qiehuan .qiehuan-top a').on('mouseover', function() {
+        $(this).css({ "color": "#FF5000" }).siblings('a').css({ "color": "#000" });
+        $('.qiehuan div:nth-child(2) a').html($(this).attr('data-inner'));
+    });
+}(jQuery);
+
+//楼梯
+! function($) {
+    function scroll() {
+        let top = $(window).scrollTop();
+        top > 500 ? $('.louti').show() : $('.louti').hide();
+        $('.louceng').each(function(index, element) {
+            let loucengtop = $(this).offset().top;
+            if (loucengtop >= top) {
+                // $('.louti .lounav').removeClass('active');
+                $('.louti .lounav').eq($(this).index()).addClass('active');
+                return false;
+            }
+        });
+    }
+    scroll();
+    $(window).on('scroll', function() {
+        scroll();
+    });
+    $('.louti .lounav').on('click', function() {
+        $(this).addClass('active').siblings().removeClass('active');
+        let loucengtop = $('.louceng').eq($(this).index()).offset().top;
+        $('html').stop(true).animate({
+            scrollTop: loucengtop
+        })
+    });
+    $('.to-top').on('click', function() {
+        $('html').stop(true).animate({
+            scrollTop: 0
+        })
+    });
+}(jQuery);
+
+// 悬浮
+$(window).on('scroll', function() {
+    let $top = $(window).scrollTop(); //滚动条顶部的偏移
+    if ($top >= 300) {
+        $('.xuanfu-search').stop(true).animate({
+            top: 0
+        });
+    } else {
+        $('.xuanfu-search').stop(true).animate({
+            top: -60
+        });
+    }
+});
